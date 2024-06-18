6379ec455871e10c9f75bdc3f080cda0cea92d4d
Cz <CaoZheng2000319@163.com>
2024-06-18 16:27:34 +0800

    final lab1

--- user/pingpong.c
+++ user/pingpong.c
@@ -2,30 +2,64 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-int main(){
-    int pid;
-    int pipes1[2];
-    int pipes2[2];
-    char buf[] = {'a'};
-    pipe(pipes1);
-    pipe(pipes2);
+#define RD 0
+#define WR 1
 
-    int ret = fork();
-    if(ret == 0){
-        pid = getpid();
-        close(pipes1[1]);
-        close(pipes2[0]);
-        read(pipes1[0], buf, 1);
-        printf("%d: receive ping\n", pid);
-        write(pipes2[1], buf, 1);
-        exit(0);
+
+int main(int argc, char const *argv[]){
+    char buf = 'P';
+    int fd_c2p[2];
+    int fd_p2c[2];
+    pipe(fd_c2p);
+    pipe(fd_p2c);
+
+    int pid = fork();
+    int exit_status = 0;
+
+    if(pid < 0){
+        fprintf(2, "fork() errro!\n");
+        close(fd_c2p[RD]);
+        close(fd_c2p[WR]);
+        close(fd_p2c[RD]);
+        close(fd_p2c[WR]);
+        exit(1);
+    }else if(pid == 0){
+        close(fd_c2p[RD]);
+        close(fd_p2c[WR]);
+
+        if(read(fd_p2c[RD], &buf, sizeof(char)) != sizeof(char)){
+            fprintf(2, "child read() error!\n");
+            exit_status = 1;
+        }else{
+            fprintf(1, "%d: received ping\n", getpid());
+        }
+
+        if(write(fd_c2p[WR], &buf, sizeof(char)) != sizeof(char)){
+            fprintf(2, "child write(), error!\n");
+            exit_status = 1;
+        }
+
+        close(fd_p2c[RD]);
+        close(fd_c2p[WR]);
+        exit(exit_status);
     }else{
-        pid = getpid();
-        close(pipes1[0]);
-        close(pipes2[1]);
-        write(pipes1[1], buf, 1);
-        read(pipes2[0], buf, 1);
-        printf("%d: receive pong\n", pid);
-        exit(0);
+        close(fd_c2p[WR]);
+        close(fd_p2c[RD]);
+
+        if(write(fd_p2c[WR], &buf, sizeof(char)) != sizeof(char)){
+            fprintf(2, "parent write() error!\n");
+            exit_status = 1;
+        }
+
+        if(read(fd_c2p[RD], &buf, sizeof(char)) != sizeof(char)){
+            fprintf(2, "parent read(), error!\n");
+            exit_status = 1;
+        }else{
+            fprintf(1, "%d: received pong\n", getpid());
+        }
+
+        close(fd_p2c[RD]);
+        close(fd_c2p[WR]);
+        exit(exit_status);
     }
 }
